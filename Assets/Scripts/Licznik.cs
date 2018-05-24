caec9e9b5febd9fe7cce05530265e25f05bea5a5
JJDrago <38635008+JJDrago@users.noreply.github.com>
2018-05-24 22:36:48 +0200

    Prawie
    Działająca wersja

---
+++ Assets/Scripts/Licznik.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Licznik : MonoBehaviour
+{
+
+    public UnityEngine.UI.Text licznikPunktow;  //Nasz licznik punktów na ekranie
+    public int punkty = 0;      // startowa ilość punktów
+
+
+    void Update()
+    {
+        licznikPunktow.text = "Points: " + punkty;      //Tekst naszego licznika to słowo "punkty" i ich wartość
+        if (Input.touchCount == 1)
+        {               //Jeżeli został wykryty dotyk
+            if (Input.GetTouch(0).phase == TouchPhase.Began)
+            {                               //dodajemy punkty tylko za "początek" dotykania. Inaczej punkty dodawały by się przez cały okres trzymania palca na ekranie.
+                punkty++;
+            }
+        }
+       
+    }
+
+}
---
+++ Assets/Scripts/TouchManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TouchManager : MonoBehaviour {
+
+	public Text tCount; //tekst z wartością punktową
+
+	// Update is called once per frame
+	void Update () {
+		tCount.text = Input.touchCount.ToString (); //zbieramy ilość kliknięć w ekran i zmieniamy ją w ciąg znaków  (string)
+	}
+}
