7fa00587925049b3641987f81b3bcec49a1a1b3c
JJDrago <38635008+JJDrago@users.noreply.github.com>
2018-05-26 21:46:53 +0200

    Prawie
    Odliczanie prawie gotowe.
    Nie wiem co się dzieje z "Licznikiem".

--- Assets/Scripts/Licznik.cs
+++ Assets/Scripts/Licznik.cs
@@ -14,13 +14,13 @@ public class Licznik : MonoBehaviour
         
     void Start()
     {
-        rekordNapis.text = "HIGH SCORE: " + PlayerPrefs.GetInt("Rekord", 0).ToString();
+        rekordNapis.text = "HIGH SCORE: " + PlayerPrefs.GetInt("Rekord", 0).ToString(); //Wczytujemy zapisany wynik gracza
     }
     
     void Update()
     {
         licznikPunktow.text = "Points: " + punkty;      //Tekst naszego licznika to słowo "punkty" i ich wartość
-        if (Input.touchCount == 1)
+        if (Input.touchCount == 1 && Time.timeScale > 0)
         {               //Jeżeli został wykryty dotyk
             if (Input.GetTouch(0).phase == TouchPhase.Began)
             {                               //dodajemy punkty tylko za "początek" dotykania. Inaczej punkty dodawały by się przez cały okres trzymania palca na ekranie.
@@ -30,11 +30,12 @@ public class Licznik : MonoBehaviour
                
     }
 
-    public void Rekord()
+    public void Rekord()        //tu robimy zapisywanie rekordu. Jest ono wywoływane przez TouchManager
     {
         if (punkty > PlayerPrefs.GetInt("Rekord", 0))
         {
-            PlayerPrefs.SetInt("Rekord", punkty);
+            PlayerPrefs.SetInt("Rekord", punkty);       //zapis rekordu
+            rekordNapis.text = "HIGH SCORE: " + PlayerPrefs.GetInt("Rekord", 0).ToString(); //odświerzenie w przypadku pobicia rekordu
         }
 
     }
--- Assets/Scripts/TimerScript.cs
+++ Assets/Scripts/TimerScript.cs
@@ -2,35 +2,84 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class TimerScript : MonoBehaviour {
 
 	Image PasekCzasu;       //grafika paska
 	public float maxTime = 10f;	//maksymalny czas do ustawienia w inspektorze
-	float timeLeft;		// pozostały czas
-	public GameObject napisTimesUp; //napis na ekranie
+	float timeLeft = 20f;		// pozostały czas
+	public GameObject menu; //napis na ekranie
+   // public GameObject restartButton; //przycisk resetu
+    //public GameObject backButton; // przycisk wyjścia
+    //public GameObject napisRekord;
 
     public Licznik _Licznik;
+    public Text odliczanie;
+    
 
 	// Use this for initialization
+
 	void Start () {
-		napisTimesUp.SetActive (false); //zaczynamy z wyłączonym napisem
-		PasekCzasu = GetComponent<Image> (); //ustawiamy pasek
-		timeLeft = maxTime; //startowy czas = maksymalny czas
-		
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {             //uniezależniamy czas od ilości klatek na sekundę
-		if (timeLeft > 0) {			//Zanim czas się skończy
-			timeLeft -= Time.deltaTime;  //odejmujemy upływająco czas
-			PasekCzasu.fillAmount = timeLeft / maxTime; // zapełnienie paska równa się pozostałem czasowi
-		} else {
-			napisTimesUp.SetActive (true); //włączamy napis na ekranie
-			Time.timeScale = 0; //zatrzymujemy upływ czasu, bo gra się skończyła
-            _Licznik.Rekord();
-		}
-		
-	}
+
+        menu.SetActive(false); //zaczynamy z wyłączonym napisem
+       // restartButton.SetActive(false); // zaczynamy bez przycisku resetu
+       // backButton.SetActive(false); // zaczynamy bez przycisku resetu
+       // napisRekord.SetActive(false);
+        PasekCzasu = GetComponent<Image>(); //ustawiamy pasek
+        StartCoroutine(Countdown(3));  
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+        //uniezależniamy czas od ilości klatek na sekundę
+        if (timeLeft > 0)
+        {           //Zanim czas się skończy
+            timeLeft -= Time.deltaTime;  //odejmujemy upływająco czas
+            PasekCzasu.fillAmount = timeLeft / maxTime; // zapełnienie paska równa się pozostałemu czasowi
+        }
+        else
+        {
+            menu.SetActive(true); //włączamy napis na ekranie
+           // restartButton.SetActive(true); //jak wyżej
+          //  quitButton.SetActive(true); // zaczynamy bez przycisku resetu
+          //  napisRekord.SetActive(true); // zaczynamy bez przycisku resetu
+            Time.timeScale = 0; //zatrzymujemy upływ czasu, bo gra się skończyła
+            _Licznik.Rekord();      //wywołujemy funkcję ze skryptu Licznik
+        }
+
+    }
+
+    IEnumerator Countdown(int seconds)
+    {
+
+        int count = seconds;
+
+        while (count > 0)
+        {
+            odliczanie.text = count.ToString();
+
+            yield return new WaitForSeconds(1);
+            count--;
+            if (count == 0)
+            {
+                odliczanie.text = "GO!";
+                yield return new WaitForSeconds(1);
+                odliczanie.text = "";
+            }
+
+
+        }
+
+        
+        
+        timeLeft = maxTime; //startowy czas = maksymalny czas
+
+    }
+
+
+
 
 }
